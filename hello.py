8a801efdb1d10bcd2d061d4bae12276a3fbcd1b6
miamayixuan <mayixuanli@yahoo.com>
2020-01-10 11:12:14 -0500

    add hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,8 @@
 # this is a sample comment
 # add your own comments to describe what this script does
 import sys
+
 name = sys.stdin.read()
+
+#Print a greeting with user' name
 print("Hello " + name + "!")
