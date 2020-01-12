a109851390241e00439aa52aad5f9a54d3a32df1
miamayixuan <mayixuanli@yahoo.com>
2020-01-12 18:38:43 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# four functions
+
+
+def multiply(a,b):
+	return a * b
+
+def add(a,b):
+	return a + b
+
+def subtract(a,b):
+	return a - b
+
+def divide(a,b):
+	return a/b
+
+
